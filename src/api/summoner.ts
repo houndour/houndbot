f98ae88ddf3980b9d166320b57abcd4b4444c02c
Lucas Godoy <lcsdegodoy@outlook.com>
2019-07-12 14:52:20 -0300

    refactor(api): remove try catch from api methods

--- src/api/summoner.ts
+++ src/api/summoner.ts
@@ -7,13 +7,8 @@ export class SummonerApi {
    * @param {string} name - The name of the summoner
    */
   static async getSummonerByName(name: string): Promise<Summoner> {
-    try {
-      const response = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.API_KEY}`);
-      return response.data;
-    }
-    catch (err) {
-      logger('summoner', err, 'error');
-    }
+    const response = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.API_KEY}`);
+    return response.data;
   }
 
   /**
@@ -21,12 +16,7 @@ export class SummonerApi {
    * @param {string} id - The id of the summoner
    */
   static async getSummonerRank(id: string): Promise<SummonerLeague[]> {
-    try {
-      const response = await axios.get(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${process.env.API_KEY}`);
-      return response.data;
-    }
-    catch (err) {
-      logger('summoner', err, 'error');
-    }
+    const response = await axios.get(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${process.env.API_KEY}`);
+    return response.data;
   }
 }
