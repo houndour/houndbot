5cdf7c5b3f1820f55b7acde27cbd36a7ca7ed4f1
Lucas Godoy <lcsdegodoy@outlook.com>
2019-07-15 14:46:18 -0300

    fix(command): release command could release someone else's champion

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -7,33 +7,35 @@ export default {
   name: 'release',
   description: 'Release one of your champions',
   async execute(message: Discord.Message, args: string[]) {
-    let user = await User.findOne({ where: { discordId: message.author.id } });
-    if (!user) {
-      message.reply('you don\'t have chosen a champion yet');
-      return;
-    }
-
     if (args.length < 1) {
       message.reply('you forgot the name of the champion');
       return;
     }
 
     const championName = args.join(' ');
-    const userChampion = await UserChampion.findOne({
+    let user = await User.findOne({
+      where: {
+        discordId: message.author.id,
+      },
       include: [{
-        model: Champion,
-        where: {
-          name: championName,
-        },
+        model: UserChampion,
+        include: [{
+          model: Champion,
+          where: {
+            name: championName,
+          },
+        }],
       }],
     });
 
-    if (!userChampion) {
+    if (!user) {
       message.reply('you don\'t have this champion');
       return;
     }
 
+    const userChampion = user.champions.find((c) => { return c.champion.name == championName });
     userChampion.destroy();
+
     message.reply(`you released ${userChampion.champion.name} (level: ${userChampion.level})`);
   }
 };
