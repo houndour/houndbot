23ef1e103ab582019dd1fa28b2ed6653f50faa4a
Lucas Godoy <lcsdegodoy@outlook.com>
2019-07-15 16:03:25 -0300

    style(command): add newline at end and convert tab to spaces

--- src/commands/champions.ts
+++ src/commands/champions.ts
@@ -4,39 +4,39 @@ import UserChampion from '../models/user-champion';
 import Champion from '../models/champion';
 
 export default {
-	name: 'champions',
-	description: 'List all champions acquired',
-	async execute(message: Discord.Message, args: string[]) {
-		let user = await User.findOne({
-			where: {
-				discordId: message.author.id,
-			},
-			include: [{
-				model: UserChampion,
-				include: [{
-					model: Champion,
-				}],
-			}],
-		});
+  name: 'champions',
+  description: 'List all champions acquired',
+  async execute(message: Discord.Message, args: string[]) {
+    let user = await User.findOne({
+      where: {
+        discordId: message.author.id,
+      },
+      include: [{
+        model: UserChampion,
+        include: [{
+          model: Champion,
+        }],
+      }],
+    });
 
-		if (!user) {
-			message.reply('You have not started your journey yet. Start with the command: Start.');
-			return;
-		}
+    if (!user) {
+      message.reply('You have not started your journey yet. Start with the command: Start.');
+      return;
+    }
 
-		if (!user.champions) {
-			message.reply('You do not have any Champions yet.');
-			return;
-		}
+    if (!user.champions) {
+      message.reply('You do not have any Champions yet.');
+      return;
+    }
 
-		const menssage = new Discord.RichEmbed()
-			.setTitle("Champions")
-			.setColor("#0099ff");
+    const menssage = new Discord.RichEmbed()
+      .setTitle("Champions")
+      .setColor("#0099ff");
 
-		for (const c of user.champions) {
-			menssage.addField(c.champion.name, `Level: ${c.level}`)
-		}
+    for (const c of user.champions) {
+      menssage.addField(c.champion.name, `Level: ${c.level}`)
+    }
 
-		message.channel.send(menssage);
-	}
+    message.channel.send(menssage);
+  }
 }
