3c97cf85f355eaa45f8b337de9983b9ffb7dc491
Lucas Godoy <lcsdegodoy@outlook.com>
2019-07-15 10:11:39 -0300

    feat(command): add command to choose starter champion

---
+++ src/commands/choose.ts
@@ -0,0 +1,43 @@
+import Discord from 'discord.js';
+import User from '../models/user';
+import Champion from '../models/champion';
+import UserChampion from '../models/user-champion';
+
+export default {
+  name: 'choose',
+  description: 'Choose your first champion to start your journey',
+  async execute(message: Discord.Message, args: string[]) {
+    let user = await User.findOne({ where: { discord_id: message.author.id } });
+    if (user) {
+      message.reply('you\'ve already chosen a champion');
+      return;
+    }
+
+    const championName = args.join(' ');
+    const champion = await Champion.findOne({
+      where: {
+        is_starter: true,
+        name: championName,
+      },
+    });
+
+    if (!champion) {
+      message.reply('that\'s not a starter champion');
+      return;
+    }
+
+    user = await User.create({
+      discordId: message.author.id, champions: [{
+        championId: champion.id,
+        level: 1,
+        experience: 0,
+      }]
+    }, {
+        include: [
+          UserChampion,
+        ]
+      });
+
+    message.reply(`congratulations! You've chosen ${champion.name} as your starter champion.`);
+  }
+};
