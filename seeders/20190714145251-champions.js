972fc56ed9293c101998ad86de6910a93459556b
Lucas Godoy <lcsdegodoy@outlook.com>
2019-07-19 13:06:55 -0300

    chore(seed): add champion_abilities seeder

--- seeders/20190714145251-champions.js
+++ seeders/20190714145251-champions.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('champions', [{
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('champions', [{
       id: 1,
       name: 'Annie',
       title: 'The dark child',
@@ -18,9 +18,79 @@ module.exports = {
       title: 'The colossus',
       is_starter: true,
     }], {});
+
+    await queryInterface.bulkInsert('champion_abilities', [
+      // annie
+      {
+        champion_id: 1,
+        name: 'Disintegrate',
+        damage: 80,
+        cooldown: 0,
+        cost: 60,
+      },
+      {
+        champion_id: 1,
+        name: 'Incinerate',
+        damage: 85,
+        cooldown: 1,
+        cost: 70,
+      },
+      {
+        champion_id: 1,
+        name: 'Summon: Tibbers',
+        damage: 280,
+        cooldown: 4,
+        cost: 100,
+      },
+      // olaf
+      {
+        champion_id: 2,
+        name: 'Undertow',
+        damage: 75,
+        cooldown: 0,
+        cost: 70,
+      },
+      {
+        champion_id: 2,
+        name: 'Vicious Strikes',
+        damage: 25,
+        cooldown: 0,
+        cost: 10,
+      },
+      {
+        champion_id: 2,
+        name: 'Reckless Swing',
+        damage: 125,
+        cooldown: 3,
+        cost: 70,
+      },
+      // galio
+      {
+        champion_id: 3,
+        name: 'Winds of War',
+        damage: 90,
+        cooldown: 0,
+        cost: 85,
+      },
+      {
+        champion_id: 3,
+        name: 'Justice Punch',
+        damage: 110,
+        cooldown: 2,
+        cost: 90,
+      },
+      {
+        champion_id: 3,
+        name: 'Hero\'s Entrance',
+        damage: 200,
+        cooldown: 5,
+        cost: 100,
+      },
+    ], {});
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('champions', null, {});
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('champions', null, {});
+    await queryInterface.bulkDelete('champion_abilities', null, {});
   }
 };
